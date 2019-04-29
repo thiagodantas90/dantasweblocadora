3f4a32760c8be69685a42c67e66cfbc2b240713c
thiago <thiago@thiago-J3060NH>
2019-04-28 22:09:45 -0300

    validacao

--- src/main/java/com/dantas/locadora/GerenciarFilmes.java
+++ src/main/java/com/dantas/locadora/GerenciarFilmes.java
@@ -3,7 +3,7 @@ package com.dantas.locadora;
 import java.util.ArrayList;
 import javax.faces.bean.ManagedBean;
 import com.dantas.locadora.ModeloFilme;
-import java.sql.Date;
+import java.util.Date;
 import javax.faces.bean.SessionScoped;
 
 /*
